6c9378e49d8c93d14ce5ce767543f6d6e872cf80
Gonzalo Sabatino <gonzalosabatino@gsabatino.local>
2023-05-12 15:32:02 -0300

    refactor: receiver message_eof

--- server/receiver/utils.py
+++ server/receiver/utils.py
@@ -1,8 +1,4 @@
 from protocol.message_client import MessageClient
-from server.eof_manager.common.message_eof import MessageEOF
-
-def eof_msg(header):
-	return MessageEOF.eof(header.data_type)
 
 def is_eof(header):
 	return header.msg_type == MessageClient.SEND_LAST
