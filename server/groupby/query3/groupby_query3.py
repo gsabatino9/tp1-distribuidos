b85594d042d4c0d4e234663a775fbce4a94e510f
Gonzalo Sabatino <gonzalosabatino@gsabatino.local>
2023-05-13 15:24:52 -0300

    fix bug groupby: suma de pesos negativos

--- server/groupby/query3/groupby_query3.py
+++ server/groupby/query3/groupby_query3.py
@@ -3,7 +3,7 @@ from haversine import haversine
 
 class GroupbyQuery3:
 	def __init__(self, name_recv_queue, name_em_queue, name_send_queue):
-		operation = lambda old, new: [old[0]+new, old[1]+1]
+		operation = lambda old, new: [old[0]+max(new,0), old[1]+1]
 		base_data = [0,0]
 
 		self.groupby_controller = GroupbyController(name_recv_queue, name_em_queue, name_send_queue, operation, base_data, self.gen_key_value)
