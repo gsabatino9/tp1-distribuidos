4b4d808a23e9ceeada8afe98641b0ae4ad950343
Gonzalo Sabatino <gonzalosabatino@gsabatino.local>
2023-05-14 18:19:33 -0300

    archivo de inicialización

--- create_docker_compose.py
+++ create_docker_compose.py
@@ -1,71 +1,43 @@
-import sys
+import sys, json
 from init_config import *
-
-NAME_STATIONS_QUEUE = "joiner_stations_q"
-NAME_WEATHER_QUEUE = "joiner_weather_q"
-NAME_TRIPS_QUEUES = ["join_trip_weather_q", "join_trip_stations_q"]
-NAME_EM_JOINERS_QUEUE = "eof_manager_joiners_q"
-NAME_FILTER_STATIONS_QUEUE = "filter_joined_stations_q"
-NAME_FILTER_WEATHER_QUEUE = "filter_joined_weather_q"
 
 
 def main():
-    num_filters_pretoc = int(sys.argv[1])
-    num_filters_year = int(sys.argv[2])
-    num_filters_distance = int(sys.argv[3])
-    num_appliers_query1 = int(sys.argv[4])
-    num_appliers_query2 = int(sys.argv[5])
-    num_appliers_query3 = int(sys.argv[6])
+    with open("config.json") as f:
+        json_config = json.load(f)
+
+    queues = json_config["config"]["queues"]
+    em_queues = json_config["config"]["eof_manager_queues"]
+    amount_nodes = json_config["config"]["amount_nodes"]
 
     receiver = RECEIVER.format(
-        NAME_STATIONS_QUEUE,
-        NAME_WEATHER_QUEUE,
-        NAME_TRIPS_QUEUES,
-        NAME_EM_JOINERS_QUEUE,
+        queues["joiners"]["stations"],
+        queues["joiners"]["weather"],
+        [
+            queues["joiners"]["join_trip_stations"],
+            queues["joiners"]["join_trip_weather"],
+        ],
+        em_queues["joiners"],
     )
     joiner_stations = JOINER_STATIONS.format(
-        NAME_STATIONS_QUEUE,
-        NAME_TRIPS_QUEUES[1],
-        NAME_EM_JOINERS_QUEUE,
-        NAME_FILTER_STATIONS_QUEUE,
+        queues["joiners"]["stations"],
+        queues["joiners"]["join_trip_stations"],
+        em_queues["joiners"],
+        queues["filters"]["filter_trip_stations"],
     )
     joiner_weather = JOINER_WEATHER.format(
-        NAME_WEATHER_QUEUE,
-        NAME_TRIPS_QUEUES[0],
-        NAME_EM_JOINERS_QUEUE,
-        NAME_FILTER_WEATHER_QUEUE,
+        queues["joiners"]["weather"],
+        queues["joiners"]["join_trip_weather"],
+        em_queues["joiners"],
+        queues["filters"]["filter_trip_weather"],
     )
 
-    filters_pretoc = ""
-    for i in range(1, num_filters_pretoc + 1):
-        filters_pretoc += FILTER_PRETOC.format(i, i)
-
-    filters_year = ""
-    for i in range(1, num_filters_year + 1):
-        filters_year += FILTER_YEAR.format(i, i)
-
-    filters_distance = ""
-    for i in range(1, num_filters_distance + 1):
-        filters_year += FILTER_DISTANCE.format(i, i)
-
-    em_filters = EM_FILTERS.format(
-        [num_filters_pretoc, num_filters_year, num_filters_distance]
+    filters_pretoc, filters_year, filters_distance, em_filters = init_filters(
+        amount_nodes
     )
 
-    appliers_query1 = ""
-    for i in range(1, num_appliers_query1 + 1):
-        appliers_query1 += APPLIER_QUERY1.format(i, i)
-
-    appliers_query2 = ""
-    for i in range(1, num_appliers_query2 + 1):
-        appliers_query2 += APPLIER_QUERY2.format(i, i)
-
-    appliers_query3 = ""
-    for i in range(1, num_appliers_query3 + 1):
-        appliers_query3 += APPLIER_QUERY3.format(i, i)
-
-    em_appliers = EM_APPLIERS.format(
-        [num_appliers_query1, num_appliers_query2, num_appliers_query3]
+    appliers_query1, appliers_query2, appliers_query3, em_appliers = init_appliers(
+        amount_nodes
     )
 
     compose = (
@@ -91,6 +63,46 @@ def main():
 
     with open("docker-compose-server.yaml", "w") as compose_file:
         compose_file.write(compose)
+
+
+def init_filters(amount_nodes):
+    filters_pretoc = ""
+    for i in range(1, amount_nodes["filter_pretoc"] + 1):
+        filters_pretoc += FILTER_PRETOC.format(i, i)
+
+    filters_year = ""
+    for i in range(1, amount_nodes["filter_year"] + 1):
+        filters_year += FILTER_YEAR.format(i, i)
+
+    filters_distance = ""
+    for i in range(1, amount_nodes["filter_distance"] + 1):
+        filters_year += FILTER_DISTANCE.format(i, i)
+
+    em_filters = EM_FILTERS.format(
+        [amount_nodes[k] for k in amount_nodes if "filter" in k]
+    )
+
+    return filters_pretoc, filters_year, filters_distance, em_filters
+
+
+def init_appliers(amount_nodes):
+    appliers_query1 = ""
+    for i in range(1, amount_nodes["applier_query1"] + 1):
+        appliers_query1 += APPLIER_QUERY1.format(i, i)
+
+    appliers_query2 = ""
+    for i in range(1, amount_nodes["applier_query2"] + 1):
+        appliers_query2 += APPLIER_QUERY2.format(i, i)
+
+    appliers_query3 = ""
+    for i in range(1, amount_nodes["applier_query3"] + 1):
+        appliers_query3 += APPLIER_QUERY3.format(i, i)
+
+    em_appliers = EM_APPLIERS.format(
+        [amount_nodes[k] for k in amount_nodes if "applier" in k]
+    )
+
+    return appliers_query1, appliers_query2, appliers_query3, em_appliers
 
 
 if __name__ == "__main__":
